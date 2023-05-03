6657a42ca01db10f60cd182b2364f3d5c5a7c7a2
Dominik Chrastecky <dominik@chrastecky.cz>
2023-05-03 10:12:27 +0200

    Make sleeping activity an api resource

--- src/Entity/SleepingActivity.php
+++ src/Entity/SleepingActivity.php
@@ -7,7 +7,9 @@ use App\Enum\ActivityType;
 use App\Repository\SleepingActivityRepository;
 use App\Trait\BasicActivityTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Rikudou\JsonApiBundle\Attribute\ApiResource;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: SleepingActivityRepository::class)]
 #[ORM\Table(name: 'sleeping_activities')]
 class SleepingActivity implements Activity
