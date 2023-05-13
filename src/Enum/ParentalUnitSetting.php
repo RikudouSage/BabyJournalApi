faa79dbc9d3ad5148902ea0db08242a1ce82fcd6
Dominik Chrastecky <dominik@chrastecky.cz>
2023-05-13 21:40:50 +0200

    Add settings for diff calculation

--- src/Enum/ParentalUnitSetting.php
+++ src/Enum/ParentalUnitSetting.php
@@ -5,4 +5,7 @@ namespace App\Enum;
 enum ParentalUnitSetting: string
 {
     case FeedingBreakLength = 'feeding_break_length';
+    case CalculateFeedingSince = 'calculate_feeding_since';
+    case CalculatePumpingSince = 'calculate_pumping_since';
+    case CalculateSleepingSince = 'calculate_sleeping_since';
 }
