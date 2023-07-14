bed8612e513852bfd6bc2999540ab954b4a2c4cb
Dominik Chrastecky <dominik@chrastecky.cz>
2023-07-14 13:40:47 +0200

    Add option to not consider water feeding

--- src/Enum/ParentalUnitSetting.php
+++ src/Enum/ParentalUnitSetting.php
@@ -5,6 +5,7 @@ namespace App\Enum;
 enum ParentalUnitSetting: string
 {
     case FeedingBreakLength = 'feeding_break_length';
+    case ConsiderWaterFeeding = 'consider_water_feeding';
     case CalculateFeedingSince = 'calculate_feeding_since';
     case CalculatePumpingSince = 'calculate_pumping_since';
     case CalculateSleepingSince = 'calculate_sleeping_since';
